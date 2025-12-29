8ebd13d8243926de752859d3e48a0d1ca60ac443
tomiwadoesux <141045581+tomiwadoesux@users.noreply.github.com>
2025-12-29 01:01:12 -0600

    upd

--- src/components/ui/list.tsx
+++ src/components/ui/list.tsx
@@ -13,29 +13,40 @@ export const SelectableList = ({
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [offset, setOffset] = useState(0);
 
-  useInput((input, key) => {
-    if (key.upArrow) {
-      setSelectedIndex((prev) => {
-        const newIndex = Math.max(0, prev - 1);
-        if (newIndex < offset) {
-          setOffset(newIndex);
-        }
-        return newIndex;
-      });
-    }
-    if (key.downArrow) {
-      setSelectedIndex((prev) => {
-        const newIndex = Math.min(items.length - 1, prev + 1);
-        if (newIndex >= offset + limit) {
-          setOffset(newIndex - limit + 1);
-        }
-        return newIndex;
-      });
-    }
-    if ((key.return || input === "\r" || input === "\n") && onSelect) {
-      onSelect(items[selectedIndex]);
-    }
-  });
+  useInput(
+    (input, key) => {
+      if (key.upArrow) {
+        setSelectedIndex((prev) => {
+          const newIndex = Math.max(0, prev - 1);
+          if (newIndex < offset) {
+            setOffset(newIndex);
+          }
+          return newIndex;
+        });
+      }
+      if (key.downArrow) {
+        setSelectedIndex((prev) => {
+          const newIndex = Math.min(items.length - 1, prev + 1);
+          if (newIndex >= offset + limit) {
+            setOffset(newIndex - limit + 1);
+          }
+          return newIndex;
+        });
+      }
+
+      if (
+        (key.return ||
+          key.tab ||
+          input === "\r" ||
+          input === "\n" ||
+          input === " ") &&
+        onSelect
+      ) {
+        onSelect(items[selectedIndex]);
+      }
+    },
+    { isActive: true }
+  );
 
   const visibleItems = items.slice(offset, offset + limit);
 
